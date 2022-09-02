500acc70b2147e95aa7bafcde945e64562f7f896
asagiman <lagavulin.jb.dc@gmail.com>
2022-09-02 20:07:46 +0900

    css

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [],
+  theme: {
+    extend: {},
+  },
+  plugins: [
+    // Docs: https://tailwindcss.com/docs/typography-plugin
+    require("@tailwindcss/typography"),
+    // Docs: https://daisyui.com/docs
+    require("daisyui"),
+    // Docs: https://github.com/tailwindlabs/tailwindcss-forms
+    require("@tailwindcss/forms"),
+    // Docs: https://github.com/tailwindlabs/tailwindcss-aspect-ratio
+    require("@tailwindcss/line-clamp"),
+    // Docs: https://github.com/tailwindlabs/tailwindcss-line-clamp
+    require("@tailwindcss/aspect-ratio"),
+  ],
+}
